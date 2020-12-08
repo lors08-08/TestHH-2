1c980012c5f4900e08cda07e52dc5667fb61dd9e
lors08 <lorswanted@gmail.com>
2020-12-08 18:09:48 +0300

    fixed popup saved

--- src/components/MainBox/Filter/Filter.js
+++ src/components/MainBox/Filter/Filter.js
@@ -17,7 +17,7 @@ function Filter(props) {
   const theme = createMuiTheme({
     overrides: {
       root: {
-        MuiDataGridColCellCheckbox: {
+        .MuiDataGridColCellCheckbox: {
           // Some CSS
           backgroundColor: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
           borderRadius: 3,
